8a77b732b73ac9a2e75742d65f11341ef9177440
ArunArivanandam <88358701+ArunArivanandam@users.noreply.github.com>
2026-03-18 16:17:47 +0530

    buffer wrt file content added

--- file-project/app.js
+++ file-project/app.js
@@ -1,11 +1,16 @@
 const fs = require("fs/promises");
 
 (async () => {
+  const commandFileHandler = await fs.open("./command.txt", "r");
+
   const watcher = fs.watch("./command.txt");
 
   for await (const event of watcher) {
     if (event.eventType === "change") {
       console.log("The file was changed");
+      const size = (await commandFileHandler.stat()).size;
+      const content = await commandFileHandler.read(Buffer.alloc(size));
+      console.log(content);
     }
   }
 })();
