a4f275d3ec8f16b5a31d842d462f87a20b72209e
ArunArivanandam <88358701+ArunArivanandam@users.noreply.github.com>
2026-03-18 17:09:37 +0530

    read method args added

--- file-project/app.js
+++ file-project/app.js
@@ -9,7 +9,17 @@ const fs = require("fs/promises");
     if (event.eventType === "change") {
       console.log("The file was changed");
       const size = (await commandFileHandler.stat()).size;
-      const content = await commandFileHandler.read(Buffer.alloc(size));
+      const buff = Buffer.alloc(size);
+
+      const offset = 0;
+      const length = buff.byteLength;
+      const position = 0;
+      const content = await commandFileHandler.read(
+        buff,
+        offset,
+        length,
+        position,
+      );
       console.log(content);
     }
   }
