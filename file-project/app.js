1f7f570380700b0c259020d9c7c0ecae035c0321
ArunArivanandam <88358701+ArunArivanandam@users.noreply.github.com>
2026-03-18 17:22:10 +0530

    event emitter implemented

--- file-project/app.js
+++ file-project/app.js
@@ -3,24 +3,28 @@ const fs = require("fs/promises");
 (async () => {
   const commandFileHandler = await fs.open("./command.txt", "r");
 
+  commandFileHandler.on("change", async () => {
+    console.log("The file was changed");
+    const size = (await commandFileHandler.stat()).size;
+    const buff = Buffer.alloc(size);
+
+    const offset = 0;
+    const length = buff.byteLength;
+    const position = 0;
+    const content = await commandFileHandler.read(
+      buff,
+      offset,
+      length,
+      position,
+    );
+    console.log(content);
+  });
+
   const watcher = fs.watch("./command.txt");
 
   for await (const event of watcher) {
     if (event.eventType === "change") {
-      console.log("The file was changed");
-      const size = (await commandFileHandler.stat()).size;
-      const buff = Buffer.alloc(size);
-
-      const offset = 0;
-      const length = buff.byteLength;
-      const position = 0;
-      const content = await commandFileHandler.read(
-        buff,
-        offset,
-        length,
-        position,
-      );
-      console.log(content);
+      commandFileHandler.emit("change");
     }
   }
 })();
