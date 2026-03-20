e3b2d598c0d33a0c9a1ab61ea6a3a4c1d387c10c
ArunArivanandam <88358701+ArunArivanandam@users.noreply.github.com>
2026-03-20 17:55:30 +0530

    optimized streams writing

--- streams/writeMany.js
+++ streams/writeMany.js
@@ -30,22 +30,51 @@ const fs = require("node:fs/promises");
   const fileHandle = await fs.open("test.txt", "w");
   const stream = fileHandle.createWriteStream();
 
-  console.log(stream.writableHighWaterMark);
-  console.log(stream.writableLength);
+  // console.log(stream.writableHighWaterMark);
 
-  const buff = Buffer.from("this is nice");
+  // const buff = Buffer.alloc(16383, "a");
+  // console.log(stream.write(buff));
+  // console.log(stream.write(Buffer.alloc(1, "a")));
 
-  stream.write(buff);
-  stream.write(buff);
-  stream.write(buff);
-  stream.write(buff);
-  stream.write(buff);
-  console.log(buff);
-  console.log(stream.writableLength);
+  // stream.on("drain", () => {
+  //   console.log(stream.write(Buffer.alloc(1, "a")));
+  //   console.log(stream.writableLength);
 
+  //   console.log("We are now safe to write more!");
+  // });
+  // setInterval(() => {}, 1000);
   // for (let i = 0; i < 1000000; i++) {
   //   const buff = Buffer.from(`${i} `, "utf-8");
-  //   stream.write(buff);
+  //   const isStreamEmpty = stream.write(buff);
+  //   if (!isStreamEmpty) {
+  //     break;
+  //     stream.on("drain", () => {});
+  //   }
+  //   continue;
   // }
-  console.timeEnd("writeMany");
+
+  let i = 0;
+
+  const writeMany = () => {
+    while (i < 1000000) {
+      const buff = Buffer.from(`${i} `, "utf-8");
+      if (i === 999999) {
+        return stream.end(buff);
+      }
+      if (!stream.write(buff)) break;
+      i++;
+    }
+  };
+
+  writeMany();
+
+  stream.on("drain", () => {
+    console.log("Drained!!");
+    writeMany();
+  });
+
+  stream.on("finish", () => {
+    console.timeEnd("writeMany");
+    fileHandle.close();
+  });
 })();
