2680d2b5e6ce33c6f3cc2a46cde6c7e7f6a0fd49
ArunArivanandam <88358701+ArunArivanandam@users.noreply.github.com>
2026-03-20 11:47:12 +0530

    benchmarking with callback version

--- streams/writeMany.js
+++ streams/writeMany.js
@@ -1,11 +1,23 @@
-const fs = require("node:fs/promises");
+// const fs = require("node:fs/promises");
+
+// (async () => {
+//   console.time("writeMany");
+//   const fileHandle = await fs.open("test.txt", "w");
+
+//   for (let i = 0; i < 1000000; i++) {
+//     await fileHandle.write(`${i} `);
+//   }
+//   console.timeEnd("writeMany");
+// })();
+
+const fs = require("node:fs");
 
 (async () => {
   console.time("writeMany");
-  const fileHandle = await fs.open("test.txt", "w");
-
-  for (let i = 0; i < 1000000; i++) {
-    await fileHandle.write(`${1}`);
-  }
-  console.timeEnd("writeMany");
+  fs.open("test.txt", "w", (err, fd) => {
+    for (let i = 0; i < 1000000; i++) {
+      fs.writeSync(fd, `${i} `);
+    }
+    console.timeEnd("writeMany");
+  });
 })();
