d350e1883fd48d041e931ef2a52b6688208b0356
ArunArivanandam <88358701+ArunArivanandam@users.noreply.github.com>
2026-03-20 12:44:29 +0530

    benchmaring with streams

--- streams/writeMany.js
+++ streams/writeMany.js
@@ -10,15 +10,29 @@
 //   console.timeEnd("writeMany");
 // })();
 
-const fs = require("node:fs");
+// const fs = require("node:fs");
 
-(() => {
+// (() => {
+//   console.time("writeMany");
+//   fs.open("test.txt", "w", (err, fd) => {
+//     for (let i = 0; i < 1000000; i++) {
+//       const buff = Buffer.from(`${i} `, "utf-8");
+//       fs.writeSync(fd, buff);
+//     }
+//     console.timeEnd("writeMany");
+//   });
+// })();
+
+const fs = require("node:fs/promises");
+
+(async () => {
   console.time("writeMany");
-  fs.open("test.txt", "w", (err, fd) => {
-    for (let i = 0; i < 1000000; i++) {
-      const buff = Buffer.from(`${i} `, "utf-8");
-      fs.writeSync(fd, buff);
-    }
-    console.timeEnd("writeMany");
-  });
+  const fileHandle = await fs.open("test.txt", "w");
+  const stream = fileHandle.createWriteStream();
+
+  for (let i = 0; i < 1000000; i++) {
+    const buff = Buffer.from(`${i} `, "utf-8");
+    stream.write(buff);
+  }
+  console.timeEnd("writeMany");
 })();
