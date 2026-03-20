50c3bfd3dbf25cc01893dbaa218cc20b1a0b0662
ArunArivanandam <88358701+ArunArivanandam@users.noreply.github.com>
2026-03-20 15:57:43 +0530

    basic stream writing

--- streams/writeMany.js
+++ streams/writeMany.js
@@ -30,9 +30,22 @@ const fs = require("node:fs/promises");
   const fileHandle = await fs.open("test.txt", "w");
   const stream = fileHandle.createWriteStream();
 
-  for (let i = 0; i < 1000000; i++) {
-    const buff = Buffer.from(`${i} `, "utf-8");
-    stream.write(buff);
-  }
+  console.log(stream.writableHighWaterMark);
+  console.log(stream.writableLength);
+
+  const buff = Buffer.from("this is nice");
+
+  stream.write(buff);
+  stream.write(buff);
+  stream.write(buff);
+  stream.write(buff);
+  stream.write(buff);
+  console.log(buff);
+  console.log(stream.writableLength);
+
+  // for (let i = 0; i < 1000000; i++) {
+  //   const buff = Buffer.from(`${i} `, "utf-8");
+  //   stream.write(buff);
+  // }
   console.timeEnd("writeMany");
 })();
